d97b3cdd02d693ce30778a079300c37a93c77b24
Iuliia Botukh <u.botuh@yandex.ru>
2025-04-17 17:02:43 +0400

    fix_media

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -110,7 +110,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media'
+MEDIA_ROOT = '/app/backend/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
